29018798de5d64076b6833ad5be9996c3b68bd99
User <yagmurnov2005@gmail.com>
2025-03-15 23:15:13 +0400

    Refactor API and add EventBus, Ocelot configuration

--- src/ApiGateways/WebApiGateway/Web.ApiGateway/Program.cs
+++ src/ApiGateways/WebApiGateway/Web.ApiGateway/Program.cs
@@ -1,3 +1,9 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.JsonWebTokens;
+using Microsoft.IdentityModel.Tokens;
+using Ocelot.DependencyInjection;
+using Ocelot.Middleware;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +12,26 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Configuration.AddJsonFile("Configurations/ocelot.json");
+builder.Services.AddOcelot();
+
+JsonWebTokenHandler.DefaultInboundClaimTypeMap.Remove("sub");
+builder.Services
+    .AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(options =>
+    {
+        options.Authority = builder.Configuration["IdentityServerUrl"];
+        options.Audience = "GatewayAPIFullAccess";
+        options.RequireHttpsMetadata = false;
+
+        options.TokenValidationParameters = new TokenValidationParameters
+        {
+            RoleClaimType = "role",
+            NameClaimType = "sub",
+            ValidateAudience = true
+        };
+    });
 
 var app = builder.Build();
 
@@ -18,8 +44,11 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
+
+await app.UseOcelot();
 
 app.Run();
